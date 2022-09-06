6312fdc9db37bd10667e5941ff7cba6176db5ac3
serlyNazarian <serlynazarian@gmail.com>
2022-09-06 18:54:00 +0400

    testing react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tweet from "./tweet";
+import "./App.css";
 
 function App() {
+  // const [isRed, setRed] = useState(false);
+  // const [count, setCount] = useState(0);
+
+  // const increment = () => {
+  //   setCount(count + 1);
+  //   setRed(!isRed);
+  // };
+  const [users, /*setUsers*/] = useState([
+    {name: 'Ed', message: 'Hello there'},
+    {name: 'John', message: 'I am John Snow'},
+    {name: 'Traversy', message: 'I am awesome'},
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    {/* <h1 className={isRed ? "red" : ""}>Change my color!</h1>
+    <button onClick={increment}>Increment</button>
+    <h1>{count}</h1> */}
+    {users.map(user =>(
+      <Tweet name={user.name} message={user.message}/>
+    ))}
     </div>
   );
-}
+};
 
 export default App;
